5eea637d8893b70765ca2f18f1bceb6f8acad42e
izumisan <izumi.mite@gmail.com>
2018-04-22 00:23:08 +0900

    NLogを使ったサンプルの追加

---
+++ NLogSample/NLogSample2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NLog;
+
+namespace NLogSample2
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            logger.Factory.Configuration.Variables.Add( @"logFileName", @"aaa" );
+
+            logger.Info( @"info" );
+        }
+    }
+}
---
+++ NLogSample/README.md
@@ -0,0 +1,7 @@
+# NLogSample
+
+- NLogの基本的な使い方
+
+# NLogSample2
+
+- CSコード側からログファイル名を指定したサンプル
