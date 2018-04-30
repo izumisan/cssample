424bc7805620b9bf44d25504ed2736ada30c8f44
izumisan <izumi.mite@gmail.com>
2018-04-30 15:56:37 +0900

    SpeechSynthesizerSampleの追加

---
+++ SpeechSynthesizerSample/SpeechSynthesizerSapmple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Speech.Synthesis;
+
+namespace SpeechSynthesizerSapmple
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var synth = new SpeechSynthesizer();
+
+            foreach ( var elem in synth.GetInstalledVoices() )
+            {
+                var info = elem.VoiceInfo;
+
+                Console.WriteLine( $@"Name: {info.Name}" );
+                Console.WriteLine( $@"Age: {info.Age}" );
+                Console.WriteLine( $@"Gender: {info.Gender}" );
+                Console.WriteLine( $@"Culture: {info.Culture}" );
+                Console.WriteLine( $@"Description: {info.Description}" );
+                Console.WriteLine();
+
+                synth.SelectVoice( info.Name );
+
+                synth.Speak( $@"My name is {info.Name}." );
+                synth.Speak( $@"私は、{info.Name}です。" );
+            }
+        }
+    }
+}
