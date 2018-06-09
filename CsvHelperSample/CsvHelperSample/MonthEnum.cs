d1ad326415e783cffcafa35eb717c6ac60b936f6
izumisan <izumi.mite@gmail.com>
2018-06-10 02:02:38 +0900

    CsvHelperSampleの追加

---
+++ CsvHelperSample/CsvHelperSample/MonthEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CsvHelperSample
+{
+    public enum MonthEnum
+    {
+        None = 0,
+        Jan,
+        Feb,
+        Mar,
+        Apr,
+        May,
+        Jun,
+        Jul,
+        Aug,
+        Sep,
+        Oct,
+        Nov,
+        Dec
+    }
+}
---
+++ CsvHelperSample/CsvHelperSample/SampleData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvHelperSample
+{
+    public class SampleData
+    {
+        public string Str { get; set; } = string.Empty;
+        public int Int { get; set; } = 0;
+        public double Double { get; set; } = 0.0;
+        public MonthEnum Month { get; set; } = MonthEnum.None;
+        public MonthEnum Month2 { get; set; } = MonthEnum.None;
+        public MonthEnum Month3 { get; set; } = MonthEnum.None;
+    }
+}
