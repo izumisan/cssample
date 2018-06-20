2d030e1944ae158e923238c9d44d0f2746baf321
izumisan <izumi.mite@gmail.com>
2018-06-21 00:33:33 +0900

    Bootstrapperサンプルの追加

---
+++ PrismSample/BootstrapperSample/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Prism.Modularity;
+using BootstrapperSample.Views;
+
+namespace BootstrapperSample
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            ( this.Shell as Window ).Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            var catalog = this.ModuleCatalog as ModuleCatalog;
+            catalog.AddModule( typeof( HelloWorldModule.HelloWorldModule ) );
+        }
+    }
+}
---
+++ PrismSample/HelloWorldModule/HelloWorldModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using HelloWorldModule.Models;
+using HelloWorldModule.Views;
+
+namespace HelloWorldModule
+{
+    public class HelloWorldModule : IModule
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; } 
+
+        [Dependency]
+        public IRegionManager RegionManager { get; set; }
+
+        public void Initialize()
+        {
+            // ViewModelの登録
+            this.Container.RegisterType<MessageProvider>( new ContainerControlledLifetimeManager() );
+
+            // Viewの登録
+            this.Container.RegisterType<object, HelloWorldView>( nameof( HelloWorldView ) );
+
+            this.RegionManager.RequestNavigate( "MainRegion", nameof( HelloWorldView ) );
+        }
+    }
+}
