92d6bb609f624b35de6147c76bb2ec1c7938f1cf
izumisan <izumi.mite@gmail.com>
2018-06-23 20:29:34 +0900

    InteractionRequest<Notification>のサンプルコードを追加

---
+++ PrismSample/InteractionRequest/InteractionRequestApp/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Prism.Modularity;
+using InteractionRequestApp.Views;
+
+namespace InteractionRequestApp
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            ( this.Shell as Window ).Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            // モジュールをカタログに追加
+            var catalog = this.ModuleCatalog as ModuleCatalog;
+
+            catalog.AddModule( typeof( NotificationModule.NotificationModule ) );
+        }
+    }
+}
