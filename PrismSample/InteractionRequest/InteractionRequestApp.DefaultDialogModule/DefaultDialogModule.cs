0c77f915c2822c00bec2e97a475f22eda09e14e9
izumisan <izumi.mite@gmail.com>
2018-06-26 00:24:44 +0900

    DefaultDialogModuleを追加
    - System.Windows.MessageBoxを利用したモジュール

---
+++ PrismSample/InteractionRequest/InteractionRequestApp.DefaultDialogModule/DefaultDialogModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using InteractionRequestApp.DefaultDialogModule.Views;
+
+namespace InteractionRequestApp.DefaultDialogModule
+{
+    public class DefaultDialogModule : IModule
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        [Dependency]
+        public IRegionManager RegionManager { get; set; }
+
+        public void Initialize()
+        {
+            this.Container.RegisterType<object, DefaultDialogModuleView>( nameof( DefaultDialogModuleView ) );
+
+            this.RegionManager.RegisterViewWithRegion( "4thRegion", typeof( DefaultDialogModuleView ) );
+        }
+    }
+}
---
+++ PrismSample/InteractionRequest/InteractionRequestApp.DefaultDialogModule/TriggerAction/DefaultDialogAction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Interactivity;
+using Prism.Interactivity.InteractionRequest;
+
+namespace InteractionRequestApp.DefaultDialogModule.TriggerAction
+{
+    public class DefaultDialogAction : TriggerAction<DependencyObject>
+    {
+        protected override void Invoke( object parameter )
+        {
+            var args = parameter as InteractionRequestedEventArgs;
+            var content = args.Context;
+
+            if ( content is Confirmation )
+            {
+                var confirmation = content as Confirmation;
+                var result = MessageBox.Show( confirmation.Content.ToString(),
+                                              confirmation.Title,
+                                              MessageBoxButton.OKCancel,
+                                              MessageBoxImage.Question );
+                confirmation.Confirmed = ( result == MessageBoxResult.OK );
+
+                args.Callback();
+            }
+            else if ( content is Notification )
+            {
+                var notification = content as Notification;
+                MessageBox.Show( notification.Content.ToString(),
+                                 notification.Title,
+                                 MessageBoxButton.OK,
+                                 MessageBoxImage.Information );
+                args.Callback();
+            }
+            else
+            {
+            }
+        }
+    }
+}
