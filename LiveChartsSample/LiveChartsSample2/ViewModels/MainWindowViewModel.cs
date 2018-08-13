c2c5dceb55d73e7c96a7baf4b8332cc6f3f324af
izumisan <izumi.mite@gmail.com>
2018-08-13 22:27:22 +0900

    LiveChartsのサンプル追加

---
+++ LiveChartsSample/LiveChartsSample2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Mvvm;
+using Prism.Commands;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace LiveChartsSample2.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+            : base()
+        {
+            P1Command = new DelegateCommand( () => Pentagon.Add( new ObservablePoint { X = P1[0], Y = P1[1] } ) );
+            P2Command = new DelegateCommand( () => Pentagon.Add( new ObservablePoint { X = P2[0], Y = P2[1] } ) );
+            P3Command = new DelegateCommand( () => Pentagon.Add( new ObservablePoint { X = P3[0], Y = P3[1] } ) );
+            P4Command = new DelegateCommand( () => Pentagon.Add( new ObservablePoint { X = P4[0], Y = P4[1] } ) );
+            P5Command = new DelegateCommand( () => Pentagon.Add( new ObservablePoint { X = P5[0], Y = P5[1] } ) );
+            ClearCommand = new DelegateCommand( () => Pentagon.Clear() );
+            RotationRight = new DelegateCommand( () => rotation( 15.0 ) );
+            RotationLeft = new DelegateCommand( () => rotation( -15.0 ) );
+        }
+
+        private static double degToRad = Math.PI / 180.0;
+        private static double radToDeg = 180.0 / Math.PI;
+
+        public ChartValues<ObservablePoint> Pentagon { get; private set; } = new ChartValues<ObservablePoint>();
+
+        public DelegateCommand P1Command { get; private set; }
+        public DelegateCommand P2Command { get; private set; }
+        public DelegateCommand P3Command { get; private set; }
+        public DelegateCommand P4Command { get; private set; }
+        public DelegateCommand P5Command { get; private set; }
+        public DelegateCommand ClearCommand { get; private set; }
+        public DelegateCommand RotationRight { get; private set; }
+        public DelegateCommand RotationLeft { get; private set; }
+
+        public Vector P1
+        {
+            get { return DenseVector.OfArray( new double[] { 1.0, 0.0 } ); }
+        }
+
+        public Vector P2
+        {
+            get { return ( rotationMatrix( 72.0 * degToRad ) * P1 ) as Vector; }
+        }
+
+        public Vector P3
+        {
+            get { return ( rotationMatrix( 72.0 * degToRad ) * P2 ) as Vector; }
+        }
+
+        public Vector P4
+        {
+            get { return ( rotationMatrix( 72.0 * degToRad ) * P3 ) as Vector; }
+        }
+
+        public Vector P5
+        {
+            get { return ( rotationMatrix( 72.0 * degToRad ) * P4 ) as Vector; }
+        }
+
+        private DenseMatrix rotationMatrix( double angle )
+        {
+            return DenseMatrix.OfArray( new double[,]
+            {
+                { Math.Cos( angle ), -1.0 * Math.Sin( angle ) },
+                { Math.Sin( angle ), Math.Cos( angle ) }
+            } );
+        }
+
+        private void rotation( double angle )
+        {
+            foreach ( var point in Pentagon )
+            {
+                var pos = DenseVector.OfArray( new double[] { point.X, point.Y } );
+                pos = rotationMatrix( angle * degToRad ) * pos;
+
+                point.X = pos[0];
+                point.Y = pos[1];
+            }
+        }
+    }
+}
