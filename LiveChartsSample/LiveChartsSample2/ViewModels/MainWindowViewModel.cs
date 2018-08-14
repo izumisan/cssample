1a8567749d97a0534cc223e6fac09a5bb252f8cd
izumisan <izumi.mite@gmail.com>
2018-08-14 13:42:59 +0900

    update LiveChartsSample2

--- LiveChartsSample/LiveChartsSample2/ViewModels/MainWindowViewModel.cs
+++ LiveChartsSample/LiveChartsSample2/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,7 @@ using Prism.Commands;
 using LiveCharts;
 using LiveCharts.Wpf;
 using LiveCharts.Defaults;
+using LiveCharts.Configurations;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 
@@ -27,12 +28,20 @@ namespace LiveChartsSample2.ViewModels
             ClearCommand = new DelegateCommand( () => Pentagon.Clear() );
             RotationRight = new DelegateCommand( () => rotation( 15.0 ) );
             RotationLeft = new DelegateCommand( () => rotation( -15.0 ) );
+
+            // Chart No.2用
+            // X軸（横軸）にy-value、Y軸（縦軸）にx-valueをマッピング
+            Mapper = Mappers.Xy<ObservablePoint>()
+                     .X( point => point.Y )
+                     .Y( point => point.X );
         }
 
         private static double degToRad = Math.PI / 180.0;
         private static double radToDeg = 180.0 / Math.PI;
 
         public ChartValues<ObservablePoint> Pentagon { get; private set; } = new ChartValues<ObservablePoint>();
+
+        public CartesianMapper<ObservablePoint> Mapper { get; private set; }
 
         public DelegateCommand P1Command { get; private set; }
         public DelegateCommand P2Command { get; private set; }
@@ -43,27 +52,27 @@ namespace LiveChartsSample2.ViewModels
         public DelegateCommand RotationRight { get; private set; }
         public DelegateCommand RotationLeft { get; private set; }
 
-        public Vector P1
+        private Vector P1
         {
             get { return DenseVector.OfArray( new double[] { 1.0, 0.0 } ); }
         }
 
-        public Vector P2
+        private Vector P2
         {
             get { return ( rotationMatrix( 72.0 * degToRad ) * P1 ) as Vector; }
         }
 
-        public Vector P3
+        private Vector P3
         {
             get { return ( rotationMatrix( 72.0 * degToRad ) * P2 ) as Vector; }
         }
 
-        public Vector P4
+        private Vector P4
         {
             get { return ( rotationMatrix( 72.0 * degToRad ) * P3 ) as Vector; }
         }
 
-        public Vector P5
+        private Vector P5
         {
             get { return ( rotationMatrix( 72.0 * degToRad ) * P4 ) as Vector; }
         }
