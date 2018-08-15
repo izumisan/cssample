c288b2bab1ad58bd48618b713ba46c074fa49115
izumisan <izumi.mite@gmail.com>
2018-08-15 21:12:47 +0900

    IPCのサンプルプログラムを追加

---
+++ IpcSample/IpcSample.Shared/IpcData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IpcSample.Shared
+{
+    public class IpcData : MarshalByRefObject, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = null;
+
+        public IpcData()
+        {
+        }
+
+        private int _value = 0;
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if ( _value != value )
+                {
+                    _value = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private void RaisePropertyChanged( [CallerMemberName] string propertyName = "" )
+        {
+            this.PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );  // イベント発行
+        }
+    }
+}
