a23be0f434515b9af5a52f6e37c1a044022dd747
izumisan <izumi.mite@gmail.com>
2018-09-09 22:40:37 +0900

    LINQのサンプルコードを追加

---
+++ LINQSample/LINQSample/LinqTest.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace LINQSample
+{
+    [TestFixture]
+    public class LinqTest
+    {
+        [Test]
+        [Category( "複数要素の取得" )]
+        public void Whereは条件を満たす要素を全て返す()
+        {
+            var source = new List<int> { 1, 3, 5, 7, 9, 8, 6, 4, 2 };
+
+            var actual = source.Where( x => x < 5 );  // => { 1, 3, 4, 2 }
+
+            var expected = new List<int> { 1, 2, 3, 4 };
+
+            Assert.That( actual, Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "複数要素の取得" )]
+        public void Distinctは重複を除いたシーケンスを返す()
+        {
+            var source = new List<int> { 1, 2, 3, 4, 5, 1, 3, 5 };
+
+            var actual = source.Distinct();  // => { 1, 2, 3, 4, 5 }
+
+            var expected = new List<int> { 1, 2, 3, 4, 5 };
+
+            Assert.That( actual, Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "複数要素の取得" )]
+        public void Skipは先頭から指定した数の要素を除いたシーケンスを返す()
+        {
+            var source = Enumerable.Range( 0, 10 );
+
+            var actual = source.Skip( 7 );
+
+            var expected = new List<int> { 7, 8, 9 };
+
+            Assert.That( actual, Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "複数要素の取得" )]
+        public void SkipWhileは先頭から指定条件を満たす要素を除いたシーケンスを返す()
+        {
+            var source = Enumerable.Range( 0, 10 );
+
+            var actual = source.SkipWhile( x => x < 7 );
+
+            var expected = new List<int> { 7, 8, 9 };
+
+            Assert.That( actual, Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "複数要素の取得" )]
+        public void Takeは先頭から指定した数の要素のシーケンスを返す()
+        {
+            var source = Enumerable.Range( 0, 10 );
+
+            var actual = source.Take( 3 );
+
+            var expected = new List<int> { 0, 1, 2 };
+
+            Assert.That( actual, Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "複数要素の取得" )]
+        public void TakeWhileは先頭から指定条件を満たす要素のシーケンスを返す()
+        {
+            var source = Enumerable.Range( 0, 10 );
+
+            var actual = source.TakeWhile( x => x < 3 );
+
+            var expected = new List<int> { 0, 1, 2 };
+
+            Assert.That( actual, Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "集計" )]
+        public void Aggregateは畳み込み処理を行う()
+        {
+            var source = Enumerable.Range( 1, 10 );  // => { 1, 2, 3, 4, 5, 6, 7, 8, 9. 10 }
+
+            var actual = source.Aggregate( ( sum, x ) => sum + x );
+
+            Assert.That( actual, Is.EqualTo( 55 ) );
+        }
+
+        [Test]
+        [Category( "判定" )]
+        public void Allは全ての要素が指定条件を満たすか否かを判定する()
+        {
+            var source = Enumerable.Range( 0, 10 );
+
+            Assert.That( source.All( x => x < 10 ), Is.EqualTo( true ) );
+            Assert.That( source.All( x => x < 7 ), Is.EqualTo( false ) );
+        }
+
+        [Test]
+        [Category( "判定" )]
+        public void Allは空シーケンスに対してtrueとなる()
+        {
+            var source = new List<int>();
+            Assert.That( source.All( x => x < 10 ), Is.EqualTo( true ) );
+        }
+
+        [Test]
+        [Category( "判定" )]
+        public void Anyは指定条件を満たす要素が含まれているか否かを判定する()
+        {
+            var source = Enumerable.Range( 0, 10 );
+
+            Assert.That( source.Any( x => x < 7 ), Is.EqualTo( true ) );
+            Assert.That( source.Any( x => x < 0 ), Is.EqualTo( false ) );
+        }
+
+        [Test]
+        [Category( "判定" )]
+        public void Anyは空シーケンスに対してfalseとなる()
+        {
+            var source = new List<int>();
+            Assert.That( source.Any( x => x < 10 ), Is.EqualTo( false ) );
+        }
+
+        [Test]
+        [Category( "判定" )]
+        public void SequenceEqualはシーケンスに対する等価判定する()
+        {
+            var source = Enumerable.Range( 0, 3 );
+
+            Assert.That( source.SequenceEqual( new List<int> { 0, 1, 2 } ), Is.EqualTo( true ) );
+            Assert.That( source.SequenceEqual( new List<int> { 2, 1, 0 } ), Is.EqualTo( false ) );
+        }
+    }
+}
