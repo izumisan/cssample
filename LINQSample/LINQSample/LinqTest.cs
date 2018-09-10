816c2998a34ebff8caa83994fc0e93dd66e9eb18
izumisan <izumi.mite@gmail.com>
2018-09-10 23:12:46 +0900

    Single, Union, Except, Intersectを追加

--- LINQSample/LINQSample/LinqTest.cs
+++ LINQSample/LINQSample/LinqTest.cs
@@ -13,6 +13,22 @@ namespace LINQSample
     public class LinqTest
     {
         [Test]
+        [Category( "要素の取得" )]
+        public void Singleは条件を満たす唯一の要素を返す()
+        {
+            var source = new List<int> { -1, 0, 1, 2, 3 };
+            Assert.That( source.Single( x => x < 0 ), Is.EqualTo( -1 ) );
+        }
+
+        [Test]
+        [Category( "要素の取得" )]
+        public void Singleは条件を満たす要素が複数の場合は例外となる()
+        {
+            var source = Enumerable.Range( 0, 10 );
+            Assert.That( () => source.Single( x => x % 2 == 0 ), Throws.Exception.TypeOf<InvalidOperationException>() );
+        }
+
+        [Test]
         [Category( "複数要素の取得" )]
         public void Whereは条件を満たす要素を全て返す()
         {
@@ -145,6 +161,42 @@ namespace LINQSample
 
             Assert.That( source.SequenceEqual( new List<int> { 0, 1, 2 } ), Is.EqualTo( true ) );
             Assert.That( source.SequenceEqual( new List<int> { 2, 1, 0 } ), Is.EqualTo( false ) );
+        }
+
+        [Test]
+        [Category( "集合" )]
+        public void Unionは指定したシーケンスとの和集合を返す()
+        {
+            var source = new List<int> { 1, 3, 5 };
+            var second = new List<int> { 0, 1, 2, 3 };
+
+            var expected = new List<int> { 0, 1, 2, 3, 5 };
+
+            Assert.That( source.Union( second ), Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "集合" )]
+        public void Exceptは指定したシーケンスとの差集合を返す()
+        {
+            var source = new List<int> { 0, 1, 2, 3, 4, 5 };
+            var second = new List<int> { 0, 1, 2 };
+
+            var expected = new List<int> { 3, 4, 5 };
+
+            Assert.That( source.Except( second ), Is.EquivalentTo( expected ) );
+        }
+
+        [Test]
+        [Category( "集合" )]
+        public void Intersectは指定したシーケンスとの積集合を返す()
+        {
+            var source = new List<int> { 0, 1, 2, 3, 4, 5 };
+            var second = new List<int> { 4, 5, 6, 7, 8, 9 };
+
+            var expected = new List<int> { 4, 5 };
+
+            Assert.That( source.Intersect( second ), Is.EquivalentTo( expected ) );
         }
     }
 }
