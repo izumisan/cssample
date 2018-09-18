47c7c6f21347cf98fdf78ea3f1ad54325cbc06a3
izumisan <izumi.mite@gmail.com>
2018-09-18 22:28:13 +0900

    Join, GroupJoinを追加

---
+++ LINQSample/LINQSample/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSample
+{
+    public class Item
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public int CategoryId { get; set; } = 0;
+    }
+}
