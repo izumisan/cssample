6e6df9c9629aa8f98fa6da6b409e15e795af2df7
izumisan <izumi.mite@gmail.com>
2018-10-03 17:11:21 +0900

    NUnitAttributeプロジェクトを追加

---
+++ NUnitSample/NUnitAttribute/BarTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace NUnitAttribute
+{
+    [TestFixture]
+    public class BarTest
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Debug.Print( "BarTest::OneTimeSetUp()" );
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Debug.Print( "BarTest::OneTimeTearDown()" );
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Debug.Print( "BarTest::SetUp()" );
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Debug.Print( "BarTest::TearDown()" );
+        }
+
+        [Test]
+        public void テスト1()
+        {
+            Debug.Print( "BarTest::テスト1()" );
+            Assert.That( true );
+        }
+
+        [Test]
+        public void テスト2()
+        {
+            Debug.Print( "BarTest::テスト2()" );
+            Assert.That( true );
+        }
+    }
+}
---
+++ NUnitSample/NUnitAttribute/SetUpFixtureClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace NUnitAttribute
+{
+    [SetUpFixture]
+    public class SetUpFixtureClass
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Debug.Print( "SetUpFixtureClass::OneTimeSetUp()" );
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Debug.Print( "SetUpFixtureClass::OneTimeTearDown()" );
+        }
+    }
+}
