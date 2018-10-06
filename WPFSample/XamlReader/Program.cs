2c6a68760d0483bf3aea85ddbd6ad5dc2e3f67ab
izumisan <izumi.mite@gmail.com>
2018-10-06 23:42:12 +0900

    XamlReaderプロジェクトを追加

---
+++ WPFSample/XamlReader/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlReader
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            // アセンブリからXAMLのストリームを取得
+            var xaml = typeof( Person ).Assembly.GetManifestResourceStream( "XamlReader.Person.xaml" );
+
+            // XAMLからPersonオブジェクトを取得
+            var p = System.Windows.Markup.XamlReader.Load( xaml ) as Person;
+
+            Console.WriteLine( $"Birthday: { p.Birthday }" );
+        }
+    }
+}
