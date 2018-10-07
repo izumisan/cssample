b14906abc6e475610a3cb70ddc8b2c54751e66e0
izumisan <izumi.mite@gmail.com>
2018-10-07 22:37:03 +0900

    FileDialog1プロジェクトを追加

---
+++ WPFSample/FileDialog/FileDialog1/FileSelectRequestEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDialog1
+{
+    public class FileSelectRequestEventArgs : EventArgs
+    {
+        public string Title { get; set; } = string.Empty;
+        public Action<string> Callback { get; set; } = null;
+    }
+}
---
+++ WPFSample/FileDialog/FileDialog1/TriggerAction/FileSelectTriggerAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using Microsoft.Win32;
+
+namespace FileDialog1.TriggerAction
+{
+    public class FileSelectTriggerAction : TriggerAction<ContentControl>
+    {
+        protected override void Invoke( object parameter )
+        {
+            if ( parameter is FileSelectRequestEventArgs )
+            {
+                var args = parameter as FileSelectRequestEventArgs;
+
+                var dialog = new OpenFileDialog
+                {
+                    Title = args.Title,
+                    CheckFileExists = true,
+                    InitialDirectory = Environment.CurrentDirectory
+                };
+
+                if ( dialog.ShowDialog() == true )
+                {
+                    var selectedFile = dialog.FileName;
+
+                    args.Callback( selectedFile );
+                }
+            }
+        }
+    }
+}
