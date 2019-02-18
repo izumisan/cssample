44a1b08069b337a19472c68791777cfbe9664f63
izumisan <izumi.mite@gmail.com>
2019-02-18 23:29:26 +0900

    add SingleProcess by CountNotifier

---
+++ ReactivePropertySample/SingleProcessByCountNotifier/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reactive.Linq;
+using Reactive.Bindings;
+using Reactive.Bindings.Notifiers;
+using Reactive.Bindings.Extensions;
+
+namespace SingleProcessByCountNotifier
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            // ProcessCounterがEmptyの時に実行できるRxCommand
+            HeavyCommand = ProcessCounter
+                .Select( x => x == CountChangedStatus.Empty )
+                .ToReactiveCommand();
+
+            HeavyCommand.Subscribe( async _ =>
+            {
+                // usingステートメントにより、Dispose()を呼び出す
+                // Dispose()により、Incrementの増加分が取り消される
+                using ( ProcessCounter.Increment() )
+                {
+                    await heavyProcesss();  // 重い処理
+                }
+            } );
+        }
+
+        public CountNotifier ProcessCounter { get; private set; } = new CountNotifier();
+
+        public ReactiveCommand HeavyCommand { get; private set; } = null;
+
+        private async Task heavyProcesss()
+        {
+            await Task.Delay( TimeSpan.FromSeconds( 5.0 ) );
+        }
+    }
+}
