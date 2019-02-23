11be6445700c725a7b4f5dd7a95b504f448a7967
izumisan <izumi.mite@gmail.com>
2019-02-23 22:52:06 +0900

    add IObservableSequence

---
+++ IObservableSquence/IObservableSequence/ValueUpdatedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IObservableSequence
+{
+    public class ValueUpdatedEventArgs : EventArgs
+    {
+        public ValueUpdatedEventArgs()
+            : base()
+        {
+        }
+
+        public int Value { get; set; } = 0;
+    }
+}
