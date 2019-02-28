d466fd2c8926b2de992bb0e8ac0ea05bb9cf90ab
izumisan <izumi.mite@gmail.com>
2019-03-01 00:49:49 +0900

    add DataTriggerAnimation

---
+++ AnimationSample/DataTriggerAnimation/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Reactive.Bindings;
+
+namespace DataTriggerAnimation
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        public ReactiveProperty<bool> Checked { get; set; } = new ReactiveProperty<bool>();
+    }
+}
