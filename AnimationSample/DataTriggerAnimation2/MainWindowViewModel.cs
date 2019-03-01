717f7e682cc6ff7d9fc95a978346a728014bf119
izumisan <izumi.mite@gmail.com>
2019-03-02 00:30:05 +0900

    add DataTriggerAnimation2

---
+++ AnimationSample/DataTriggerAnimation2/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Mvvm;
+
+namespace DataTriggerAnimation2
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+            : base()
+        {
+        }
+
+        private bool _checked = false;
+
+        public bool Checked
+        {
+            get { return _checked; }
+            set { SetProperty( ref _checked, value ); }
+        }
+    }
+}
