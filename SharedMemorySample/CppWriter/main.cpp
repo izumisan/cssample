cbffa82baf4811196ac10b8adbfc8ba42297784d
izumisan <izumi.mite@gmail.com>
2019-04-15 23:06:34 +0900

    change to write async

--- SharedMemorySample/CppWriter/main.cpp
+++ SharedMemorySample/CppWriter/main.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
+#include <string>
+#include <future>
 #include <thread>
 
 #include "sharedmemory.h"
 
 int main()
 {
-    auto&& shm = SharedMemory();
+    std::cout << "<<< Quit on press Enter key >>>" << std::endl;
 
-    Foo wdata = {};
-
-    int count = 0;
-    while ( count < 10 )
+    bool exitFlag = false;
+    std::future<void> f = std::async( std::launch::async, [&exitFlag]
     {
-        wdata.count = count;
-        wdata.ivalue = count + 1;
-        wdata.dvalue = count + 0.777;
-        for ( int i = 0; i < 512; ++i )
+        auto&& shm = SharedMemory();
+        Foo wdata = {};
+
+        int count = 0;
+        while ( exitFlag != true )
         {
-            wdata.array[i] = count + i;
+            wdata.count = count;
+            wdata.ivalue = count + 1;
+            wdata.dvalue = count + 0.777;
+            for ( int i = 0; i < 512; ++i )
+            {
+                wdata.array[i] = count + i;
+            }
+
+            shm.write( wdata );
+
+            std::cout << "write: " << count << std::endl;
+
+            std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
+            ++count;
         }
 
+        wdata.exitFlag = 1;
         shm.write( wdata );
+    } );
 
-        std::cout << "write: " << count << std::endl;
+    std::string buff = "";
+    std::getline( std::cin, buff );
 
-        std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
-        ++count;
-    }
+    exitFlag = true;
 
-    wdata.exitFlag = 1;
-    shm.write( wdata );
+    f.wait();
 
     return 0;
 }
