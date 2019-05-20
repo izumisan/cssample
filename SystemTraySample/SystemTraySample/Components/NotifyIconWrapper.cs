488266ab286770e941bbb6443ce832b720d85492
izumisan <izumi.mite@gmail.com>
2019-05-20 22:43:38 +0900

    add SystemTraySample

---
+++ SystemTraySample/SystemTraySample/Components/NotifyIconWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemTraySample.Components
+{
+    public partial class NotifyIconWrapper : Component
+    {
+        public NotifyIconWrapper()
+        {
+            InitializeComponent();
+
+            menuItem1.Click += ( s, e ) =>
+            {
+                Debug.Print( "item1 clicked" );
+
+                var window1 = new Window1();
+                window1.Show();
+            };
+            menuItem2.Click += ( s, e ) =>
+            {
+                Debug.Print( "item2 clicked" );
+
+                var window2 = new Window2();
+                window2.Show();
+            };
+            quitMenuItem.Click += ( s, e ) =>
+            {
+                Debug.Print( "quit clicked" );
+                System.Windows.Application.Current.Shutdown();
+            };
+        }
+
+        public NotifyIconWrapper( IContainer container )
+        {
+            container.Add( this );
+
+            InitializeComponent();
+        }
+    }
+}
