0146292c3a5cb141255964d05469393ff653a779
izumisan <izumi.mite@gmail.com>
2019-05-29 22:47:22 +0900

    add Prism 7 sample

---
+++ Prism7Sample/PrismApp/PrismApp/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using PrismApp.Views;
+
+namespace PrismApp
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : PrismApplication  // AppクラスをPrismApplicationクラスのサブクラスとする
+    {
+        // Prism 7ではAppクラスをPrismApplicationクラスのサブクラスとし、
+        // Bootstrapperの代わりにCreateShellやModuleの追加を行う
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes( IContainerRegistry containerRegistry )
+        {
+        }
+
+        protected override void ConfigureModuleCatalog( IModuleCatalog moduleCatalog )
+        {
+            base.ConfigureModuleCatalog( moduleCatalog );
+
+            // カタログにモジュールを追加
+            moduleCatalog.AddModule<FooModule.FooModule>();
+        }
+    }
+}
