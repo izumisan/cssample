b7f4a4110d69f2011d10db5861a97264ae16e409
izumisan <izumi.mite@gmail.com>
2019-06-02 16:20:09 +0900

    add Json.NET sample

---
+++ JsonNETSample/JsonNETSample/Foo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace JsonNETSample
+{
+    public class Foo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty("value")]
+        public int Value { get; set; } = 0;
+
+        [JsonProperty("lucky")]
+        public bool Lucky { get; set; } = false;
+    }
+}
---
+++ JsonNETSample/JsonNETSample/JsonNETTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace JsonNETSample
+{
+    [TestFixture]
+    public class JsonNETTest
+    {
+        [Test]
+        public void シリアライズtoJSON()
+        {
+            var foo = new Foo
+            {
+                Name = "Foo 32",
+                Value = 32,
+                Lucky = true
+            };
+
+            // シリアライズ
+            string json = JsonConvert.SerializeObject( foo );  // => {"name":"Foo 32","value":32,"lucky":true}
+
+            System.Diagnostics.Debug.Print( json );
+        }
+        
+        [Test]
+        public void デシリアライズfromJSON()
+        {
+            string json = @"{ ""name"": ""foo"", ""value"": 168, ""lucky"": true}";
+
+            // デシリアライズ
+            var foo = JsonConvert.DeserializeObject<Foo>( json );
+
+            Assert.That( foo.Name, Is.EqualTo( "foo" ) );
+            Assert.That( foo.Value, Is.EqualTo( 168 ) );
+            Assert.That( foo.Lucky, Is.True );
+        }
+    }
+}
