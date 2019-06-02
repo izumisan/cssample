0b60685468f6305ac2a32b39f92893016b34fa9a
izumisan <izumi.mite@gmail.com>
2019-06-02 17:32:53 +0900

    add EnumConverter sample

---
+++ JsonNETSample/EnumSample/Foo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace EnumSample
+{
+    public class Foo
+    {
+        /// <summary>
+        /// 属性を指定していないEnumプロパティ
+        /// </summary>
+        public MonthEnum Month1 { get; set; } = MonthEnum.None;
+
+        /// <summary>
+        /// JsonConverterにStringEnumConverterを指定したEnumプロパティ
+        /// </summary>
+        [JsonConverter(typeof(StringEnumConverter))]
+        public MonthEnum Month2 { get; set; } = MonthEnum.None;
+    }
+}
