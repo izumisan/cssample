c07e523318298206151e8095750cc4dee58514c8
izumisan <izumi.mite@gmail.com>
2019-06-06 23:01:05 +0900

    add CallMethodAction sample

---
+++ WPFSample/EventToCommand/CallMethodAction/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using Reactive.Bindings;
+
+namespace CallMethodAction
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Label = new ReactiveProperty<string>("CallMethodActionにより、任意のイベント時にメソッドを呼び出す");
+        }
+
+        public ReactiveProperty<string> Label { get; private set; } = null;
+
+        public void OnMouseEnter()
+        {
+            Label.Value = "MouseEnter";
+        }
+
+        public void OnMouseLeave()
+        {
+            Label.Value = "MouseLeave";
+        }
+    }
+}
