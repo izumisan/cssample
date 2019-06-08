617bcfd9546bd814bf0dade6b665d864a701279e
izumisan <izumi.mite@gmail.com>
2019-06-08 23:28:46 +0900

    add Attribute sample

---
+++ AttributeSample/AttributeSample/FooAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeSample
+{
+    /*
+        自作属性クラスは、Attributeクラスを派生して作成する
+        AttributeUsage属性で、自作属性の付与する対象を指定する
+    */
+    [AttributeUsage( AttributeTargets.Class | AttributeTargets.Struct )]
+    public class FooAttribute : Attribute
+    {
+        public FooAttribute( string name )
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; } = string.Empty;
+    }
+}
---
+++ AttributeSample/AttributeSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+using System.Diagnostics;
+
+namespace AttributeSample
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            // Class1クラスに付与されているの属性を取得する
+            Attribute[] attributes = Attribute.GetCustomAttributes( typeof( Class1 ) );
+
+            attributes.ToList().ForEach( attr =>
+            {
+                Debug.WriteLine( attr.GetType().Name );
+
+                if ( attr is FooAttribute )
+                {
+                    var fooattr = attr as FooAttribute;
+                    Debug.Print( $"[Foo] Name: { fooattr.Name }" );
+                }
+                else if ( attr is BarAttribute )
+                {
+                    var barattr = attr as BarAttribute;
+                    Debug.Print( $"[Bar] Name: { barattr.Name }, Message: { barattr.Message }, Value: { barattr.Value }, Lucky: { barattr.Lucky }" );
+                }
+                else
+                {
+                }
+            } );
+        }
+    }
+}
