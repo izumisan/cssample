5dfe3a14f71afe498a938c9844122847a50c34e3
izumisan <izumi.mite@gmail.com>
2019-06-10 23:32:55 +0900

    add resource samples

---
+++ ResourceSample/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using ( var stream = new MemoryStream() )
+            {
+                // リソース画像（System.Drawing.Bitmap）を
+                // System.Windows.Control.ImageのSourceに設定できる型
+                // (System.Windows.Media.BitmapFrameやBitmapImage）に変換する
+                Bitmap bitmap = Properties.Resources.metalupa;
+                bitmap.Save( stream, ImageFormat.Bmp );
+                stream.Seek( 0, SeekOrigin.Begin );
+                image.Source = BitmapFrame.Create( stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad );
+            }
+        }
+    }
+}
