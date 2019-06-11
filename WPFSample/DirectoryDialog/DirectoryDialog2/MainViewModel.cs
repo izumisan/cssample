450b3a1980b46b7f7953a7ad25430a8f7d823de0
izumisan <izumi.mite@gmail.com>
2019-06-11 23:57:17 +0900

    add DirectoryDialog sample 2

---
+++ WPFSample/DirectoryDialog/DirectoryDialog2/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Reflection;
+using Prism.Mvvm;
+using Prism.Commands;
+
+namespace DirectoryDialog2
+{
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel()
+            : base()
+        {
+            OpenDirectoryCommand = new DelegateCommand( openDirectoryCommandExecute );
+        }
+
+        private string _selectedDirectory = string.Empty;
+
+        public string SelectedDirectory
+        {
+            get { return _selectedDirectory; }
+            set { SetProperty( ref _selectedDirectory, value ); }
+        }
+
+        public DelegateCommand OpenDirectoryCommand { get; private set; }
+
+        private void openDirectoryCommandExecute()
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,  // フォルダー選択ダイアログモード
+                Title = "たいとる",
+                InitialDirectory = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location )
+            };
+
+            if ( dialog.ShowDialog() == CommonFileDialogResult.Ok )
+            {
+                SelectedDirectory = dialog.FileName;
+            }
+        }
+    }
+}
