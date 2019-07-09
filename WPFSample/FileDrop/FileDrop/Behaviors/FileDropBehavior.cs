d5b2e8b947c20eafcd878e647459e9bc58c01c7b
izumisan <izumi.mite@gmail.com>
2019-07-09 23:34:58 +0900

    add file drag & drop sample

---
+++ WPFSample/FileDrop/FileDrop/Behaviors/FileDropBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace FileDrop.Behaviors
+{
+    public class FileDropBehavior : Behavior<UIElement>
+    {
+        public static readonly DependencyProperty DropFileProperty =
+            DependencyProperty.Register(
+                "DropFile",
+                typeof( string ),
+                typeof( FileDropBehavior ),
+                new PropertyMetadata( string.Empty ) );
+
+        public string DropFile
+        {
+            get { return GetValue( DropFileProperty ) as string; }
+            set { SetValue( DropFileProperty, value ); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.PreviewDragOver += AssociatedObject_PreviewDragOver;
+            AssociatedObject.Drop += AssociatedObject_Drop;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+
+            AssociatedObject.PreviewDragOver -= AssociatedObject_PreviewDragOver;
+            AssociatedObject.Drop -= AssociatedObject_Drop;
+        }
+
+        private void AssociatedObject_PreviewDragOver( object sender, DragEventArgs e )
+        {
+            if ( e.Data.GetDataPresent( DataFormats.FileDrop, true ) )
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void AssociatedObject_Drop( object sender, DragEventArgs e )
+        {
+            var dropFiles = e.Data.GetData( DataFormats.FileDrop ) as string[];
+            DropFile = dropFiles[0];
+        }
+    }
+}
---
+++ WPFSample/FileDrop/FileDrop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Mvvm;
+
+namespace FileDrop.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+            : base()
+        {
+        }
+
+        private string _dropFile = string.Empty;
+
+        public ObservableCollection<string> Files { get; } = new ObservableCollection<string>();
+
+        public string DropFile
+        {
+            get { return _dropFile; }
+            set { SetProperty( ref _dropFile, value, () => Files.Add( _dropFile ) ); }
+        }
+    }
+}
