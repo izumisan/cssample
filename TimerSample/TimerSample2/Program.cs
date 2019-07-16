4a76e633be5a547005db6638a73630514943ce64
izumisan <izumi.mite@gmail.com>
2019-07-16 22:32:48 +0900

    add System.Threading.Timer sample

---
+++ TimerSample/TimerSample2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace TimerSample2
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "start..." );
+            Debug.WriteLine( $"Main thread id: { System.Threading.Thread.CurrentThread.ManagedThreadId }" );
+
+            int count = 0;
+
+            System.Threading.TimerCallback callback = state =>
+            {
+                ++count;
+                var id = System.Threading.Thread.CurrentThread.ManagedThreadId;
+                Debug.WriteLine( $"{ DateTime.Now.ToString( "HH:mm:ss.fff" ) }, count:{ count }, id:{ id }" );
+            };
+            var timer = new System.Threading.Timer( callback, null, 0, 1000 );
+
+            Console.WriteLine( "Press any key..." );
+            Console.ReadLine();
+
+            timer.Dispose();
+            Console.WriteLine( "end." );
+        }
+    }
+}
