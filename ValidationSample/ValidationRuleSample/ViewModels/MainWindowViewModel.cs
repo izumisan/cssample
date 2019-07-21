f34860af100762ef1821dfd900e3745684f8fef8
izumisan <izumi.mite@gmail.com>
2019-07-21 23:39:20 +0900

    add ValidationRule sample

---
+++ ValidationSample/ValidationRuleSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using Prism.Mvvm;
+using Prism.Commands;
+
+namespace ValidationRuleSample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+            : base()
+        {
+        }
+
+        private string _input = string.Empty;
+
+        public string Input
+        {
+            get { return _input; }
+            set { SetProperty( ref _input, value ); }
+        }
+    }
+}
