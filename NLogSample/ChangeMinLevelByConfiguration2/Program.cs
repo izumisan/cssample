e84432190f733810da9ea03481a2b9bf7264ed1c
izumisan <izumi.mite@gmail.com>
2019-08-03 22:57:00 +0900

    add sample to change output log level by configuration 2

---
+++ NLogSample/ChangeMinLevelByConfiguration2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using NLog;
+
+namespace ChangeMinLevelByConfiguration2
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            SetupConfiguration();
+
+            ILogger logger = LogManager.GetCurrentClassLogger();
+            logger.Info( "========== START ==========" );
+            logger.Trace( "trace message" );
+            logger.Debug( "debug message" );
+            logger.Info( "info message" );
+            logger.Warn( "warn message" );
+            logger.Error( "error message" );
+            logger.Fatal( "fatal message" );
+            logger.Info( "==========  END  ==========" );
+        }
+
+        private static void SetupConfiguration()
+        {
+            LogManager.Configuration.Variables.Add( "debuglog", "ignore" );
+            SetDebulogEnabled();
+        }
+
+        [Conditional( "DEBUG" )]
+        private static void SetDebulogEnabled()
+        {
+            LogManager.Configuration.Variables["debuglog"] = "enabled";
+        }
+    }
+}
