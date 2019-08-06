b1640266638625b530ba6b236c0015c911fb13da
izumisan <izumi.mite@gmail.com>
2019-08-06 22:13:20 +0900

    [wip] add DialogService sample

---
+++ DialogSample/DialogService/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using Prism.Mvvm;
+using Prism.Commands;
+using Prism.Services.Dialogs;
+
+namespace DialogService.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel( IDialogService dialogService )
+            : base()
+        {
+            _dialogService = dialogService;
+
+            NotificationCommand = new DelegateCommand( () =>
+            {
+                _dialogService.ShowDialog(
+                    "NotificationView", 
+                    new DialogParameters(), 
+                    ( c ) => Debug.WriteLine( $"result: { c.Result }" ) );  // 閉じるボタンから閉じた場合は ButtonResult.None となる
+            } );
+
+            ConfirmationCommand = new DelegateCommand( () =>
+            {
+                Debug.Print( "bar" );
+            } );
+        }
+
+        private readonly IDialogService _dialogService = null;
+
+        public DelegateCommand NotificationCommand { get; private set; } = null;
+        public DelegateCommand ConfirmationCommand { get; private set; } = null;
+    }
+}
---
+++ DialogSample/DialogService/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Mvvm;
+using Prism.Commands;
+using Prism.Services.Dialogs;
+
+namespace DialogService.ViewModels
+{
+    public class NotificationViewModel : BindableBase, IDialogAware
+    {
+        public NotificationViewModel()
+            : base()
+        {
+            OkCommand = new DelegateCommand( () => RequestClose?.Invoke( new DialogResult( ButtonResult.OK ) ) );
+        }
+
+        #region IDialogAwareの実装
+
+        public event Action<IDialogResult> RequestClose = null;
+
+        public string Title { get; } = "たいとる";
+
+        public void OnDialogOpened( IDialogParameters parameters )
+        {
+        }
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+        #endregion
+
+        public DelegateCommand OkCommand { get; private set; } = null;
+    }
+}
