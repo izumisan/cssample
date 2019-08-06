8594c1240d2a68b0244b522f3c3eab7d6d22e838
izumisan <izumi.mite@gmail.com>
2019-08-07 00:09:20 +0900

    add private test sample

---
+++ PrivateTestSample/FooLibrary/Foo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooLibrary
+{
+    public class Foo
+    {
+        public Foo()
+        {
+        }
+
+        private int _value = 32;
+        private string _foo = "private property";
+        private string FooProperty
+        {
+            get { return _foo; }
+            set { _foo = value; }
+        }
+        private int FooMethod( int x, int y )
+        {
+            return x + y;
+        }
+
+        private static int _staticValue = 168;
+        private static string _staticFoo = "private static property";
+        private static string StaticFooProperty
+        {
+            get { return _staticFoo; }
+            set { _staticFoo = value; }
+        }
+        private static int StaticFooMethod( int x, int y )
+        {
+            return x * y;
+        }
+    }
+}
