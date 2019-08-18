6512ac3992cc40067e1583625c59839a3224ab6f
izumisan <izumi.mite@gmail.com>
2019-08-18 13:18:37 +0900

    add EventToReactiveProperty sample

---
+++ WPFSample/EventToCommand/EventToReactiveProperty/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using Reactive.Bindings;
+
+namespace EventToReactiveProperty
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        public ReactiveProperty<Point> MousePoint { get; set; } = new ReactiveProperty<Point>();
+    }
+}
