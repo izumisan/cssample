b969d228bbade17f6c924e61b2afb489005f8e1d
izumisan <izumi.mite@gmail.com>
2020-03-24 01:01:51 +0900

    New: XmlToJson sample

---
+++ XmlToJsonSample/XmlToJsonTest/XmlToJsonTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace XmlToJsonTest
+{
+    [TestFixture]
+    public class XmlToJsonTest
+    {
+        [Test]
+        public void XMLをJSONに変換する()
+        {
+            var testfile = Path.Combine( TestContext.CurrentContext.TestDirectory, "testdata", "foo.xml" );
+            Assert.That( testfile, Does.Exist );
+
+            var outfile = Path.Combine( TestContext.CurrentContext.TestDirectory, "out_foo.json" );
+            if ( File.Exists( outfile ) )
+            {
+                File.Delete( outfile );
+            }
+            Assert.That( outfile, Does.Not.Exist );
+
+
+            var xmldoc = new XmlDocument();
+            var xmlcontents = File.ReadAllText( testfile );
+            xmldoc.LoadXml( xmlcontents );
+
+            // JsonConvert.SerializeXmlNode()にXmlDocumentオブジェクトを渡せば、
+            // XMLをJsonフォーマットに変換できる
+            var jsoncontents = JsonConvert.SerializeXmlNode( xmldoc, Newtonsoft.Json.Formatting.Indented );
+
+            File.WriteAllText( outfile, jsoncontents );
+            Assert.That( outfile, Does.Exist );
+        }
+    }
+}
