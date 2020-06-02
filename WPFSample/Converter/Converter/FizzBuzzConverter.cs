c76d1cc45d4252a178412614548f93fa2bf63606
izumisan <izumi.mite@gmail.com>
2020-06-03 08:33:07 +0900

    New: Converter sample

---
+++ WPFSample/Converter/Converter/FizzBuzzConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Data;
+
+namespace Converter
+{
+    public class FizzBuzzConverter : IValueConverter
+    {
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            var ret = string.Empty;
+            if ( value.GetType() == typeof( double ) )
+            {
+                int val = (int)( (double)value );
+                if ( val % 15 == 0 )
+                {
+                    ret = "Fizz Buzz";
+                }
+                else if ( val % 5 == 0 )
+                {
+                    ret = "Buzz";
+                }
+                else if ( val % 3 == 0 )
+                {
+                    ret = "Fizz";
+                }
+                else
+                {
+                    ret = val.ToString();
+                }
+            }
+            return ret;
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
