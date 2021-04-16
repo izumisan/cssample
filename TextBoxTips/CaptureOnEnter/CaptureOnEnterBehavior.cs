b918bb28997cfd3ec55429e87f2007e338afa9bf
izumisan <izumi.mite@gmail.com>
2021-04-16 23:54:04 +0900

    Add: CaptureOnEnter behavior sample

---
+++ TextBoxTips/CaptureOnEnter/CaptureOnEnterBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Data;
+using Microsoft.Xaml.Behaviors;
+
+namespace CaptureOnEnter
+{
+    /// <summary>
+    /// Enterキー押下で、TextBox.Textプロパティのバイディングソースを更新するビヘイビア
+    /// </summary>
+    public class CaptureOnEnterBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.PreviewKeyDown += OnPreviewKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.PreviewKeyDown -= OnPreviewKeyDown;
+        }
+
+        private void OnPreviewKeyDown( object sender, KeyEventArgs e )
+        {
+            if ( ( sender is TextBox tb ) && ( e.Key == Key.Enter ) )
+            {
+                // TextBox.Textプロパティのバイディングソースを更新する
+                var binding = BindingOperations.GetBindingExpression( tb, TextBox.TextProperty );
+                binding?.UpdateSource();
+            }
+        }
+    }
+}
