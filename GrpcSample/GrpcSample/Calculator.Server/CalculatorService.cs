324c0b635ee8fd3c28672bf35eab901712732ed1
izumisan <izumi.mite@gmail.com>
2021-06-15 23:29:20 +0900

    Add: Calculator sample

---
+++ GrpcSample/GrpcSample/Calculator.Server/CalculatorService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+using Calculator;
+
+namespace Calculator.Server
+{
+    class CalculatorService : Calculator.CalculatorBase
+    {
+        public override Task<CalculatorResponse> Plus( CalculatorRequest request, ServerCallContext context )
+        {
+            return Task.FromResult( new CalculatorResponse
+            {
+                Arg1 = request.Arg1,
+                Arg2 = request.Arg2,
+                Result = request.Arg1 + request.Arg2
+            } );
+        }
+
+        public override Task<CalculatorResponse> Minus( CalculatorRequest request, ServerCallContext context )
+        {
+            return Task.FromResult( new CalculatorResponse
+            {
+                Arg1 = request.Arg1,
+                Arg2 = request.Arg2,
+                Result = request.Arg1 - request.Arg2
+            } );
+        }
+
+        public override Task<CalculatorResponse> Multiply( CalculatorRequest request, ServerCallContext context )
+        {
+            return Task.FromResult( new CalculatorResponse
+            {
+                Arg1 = request.Arg1,
+                Arg2 = request.Arg2,
+                Result = request.Arg1 * request.Arg2
+            } );
+        }
+
+        public override Task<CalculatorResponse> Devide( CalculatorRequest request, ServerCallContext context )
+        {
+            return Task.FromResult( new CalculatorResponse
+            {
+                Arg1 = request.Arg1,
+                Arg2 = request.Arg2,
+                Result = ( request.Arg2 != 0.0 ) ? ( request.Arg1 / request.Arg2 ) : double.NaN
+            } );
+        }
+    }
+}
---
+++ GrpcSample/GrpcSample/Calculator.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+
+namespace Calculator.Server
+{
+    class Program
+    {
+        public static int Port => 5678;
+
+        static void Main( string[] args )
+        {
+            var server = new Grpc.Core.Server
+            {
+                Services = { Calculator.BindService( new CalculatorService() ) },
+                Ports = { new ServerPort( "localhost", Port, ServerCredentials.Insecure ) }
+            };
+            server.Start();
+
+            Console.WriteLine( $"CalculatorService listening on port { Port }" );
+            Console.WriteLine( "Press any key to stop server..." );
+            Console.ReadLine();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
