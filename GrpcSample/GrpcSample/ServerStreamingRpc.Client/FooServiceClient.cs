2c2cdffa9e2d1cafa423d4ad414ca71a4a04b122
izumisan <izumi.mite@gmail.com>
2021-06-20 23:46:13 +0900

    Add: Server-side streaming RPC sample

---
+++ GrpcSample/GrpcSample/ServerStreamingRpc.Client/FooServiceClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+using ServerStreamingRpc;
+
+namespace ServerStreamingRpc.Client
+{
+    public class FooServiceClient
+    {
+        public FooServiceClient( FooService.FooServiceClient client )
+        {
+            _client = client;
+        }
+
+        private FooService.FooServiceClient _client = null;
+
+        public async Task<List<int>> GetFooList( int value )
+        {
+            var ret = new List<int>();
+
+            var req = new FooRequest { Value = value };
+            using ( var call = _client.GetFooList( req ) )
+            {
+                IAsyncStreamReader<FooResponse> resstream = call.ResponseStream;
+                while ( await resstream.MoveNext() )
+                {
+                    var res = resstream.Current;
+                    ret.Add( res.Value );
+                }
+            }
+
+            return ret;
+        }
+    }
+}
