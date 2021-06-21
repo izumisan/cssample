3e9bc275edeec48a64d091f99cff28c93b445932
izumisan <izumi.mite@gmail.com>
2021-06-22 00:37:04 +0900

    Add: Client-side streaming RPC sample

---
+++ GrpcSample/GrpcSample/ClientStreamingRpc.Server/FooServiceImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+
+namespace ClientStreamingRpc.Server
+{
+    class FooServiceImpl : FooService.FooServiceBase
+    {
+        public override async Task<FooResponse> Total( IAsyncStreamReader<FooRequest> requestStream, ServerCallContext context )
+        {
+            int total = 0;
+            while ( await requestStream.MoveNext() )
+            {
+                total += requestStream.Current.Value;
+            }
+            return new FooResponse { Value = total };
+        }
+    }
+}
