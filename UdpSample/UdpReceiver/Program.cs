9ec4dfcfefb9b0a0b4617e1d0c24dbd0e29e48ee
izumisan <izumi.mite@gmail.com>
2021-12-17 18:04:26 +0900

    Add: UDP sample

---
+++ UdpSample/UdpReceiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using Udp.Shared;
+
+namespace UdpReceiver
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "UdpReceiver (exit on received \"quit\")" );
+            //var udp = new UdpClient( Constants.IpAddress, Constants.Port );  // Receiverなのでこれはダメ
+            //var udp = new UdpClient( Constants.Port );  // 0.0.0.0:<port> でリッスンする（全アドレスリッスン）
+            var address = IPAddress.Parse( Constants.IpAddress );
+            var localEP = new IPEndPoint( address, Constants.Port );
+            var udp = new UdpClient( localEP );
+
+            Console.WriteLine( $"Listening... addr: { Constants.IpAddress }, port: { Constants.Port }" );
+
+            while ( true )
+            {
+                IPEndPoint remoteEP = null;
+                byte[] rdata = udp.Receive( ref remoteEP );
+
+                var msg = Encoding.UTF8.GetString( rdata );
+                Console.WriteLine( $"Rx: addr: { remoteEP.Address }, port: { remoteEP.Port }, msg: { msg }" );
+
+                if ( msg.Equals( "quit" ) )
+                {
+                    break;
+                }
+            }
+
+            udp.Close();
+        }
+    }
+}
---
+++ UdpSample/UdpSender/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using Udp.Shared;
+
+namespace UdpSender
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "UdpSender (exit if input \"quit\")" );
+            //var udp = new UdpClient();  // [1]
+            var udp = new UdpClient( Constants.IpAddress, Constants.Port );  // [2]
+
+            while ( true )
+            {
+                Console.Write( "> " );
+                var input = Console.ReadLine();
+                var bytes = Encoding.UTF8.GetBytes( input );
+
+                // UdpClientのコンストラクタでアドレスポートを指定していない場合(see. [1])
+                //udp.Send( bytes, bytes.Length, Constants.IpAddress, Constants.Port );
+
+                // UdpClientのコンストラクタでアドレス、ポート番号を指定している場合 (see. [2])
+                udp.Send( bytes, bytes.Length );
+
+                if ( input.Equals( "quit" ) )
+                {
+                    break;
+                }
+            }
+
+            udp.Close();
+        }
+    }
+}
